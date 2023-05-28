f3cfc80480be44b4a5317b43079e0d659b1d0838
pranaykumarkodam <pranay.k@blocmatrix.com>
2023-05-28 21:34:09 +0530

    otp and login auth

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 main();
+app.use((err, req, res, next) => {
+  // you can error out to stderr still, or not; your choice
+  // console.error(err);
+
+  // body-parser will set this to 400 if the json is in error
+  if (err.status === 400)
+    return res.send({ status: 400, message: "Dude, you messed up the JSON" });
+
+  return next(err); // if it's not a 400, let the default error handling do it.
+});
 
 //User Route
 app.use("/api/user", UserRoute);
